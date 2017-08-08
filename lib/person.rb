08daa2d67140afc9b9f00e6bffcbf8f1e4dd1b53
Rachel Thompson <thompr@pba.edu>
2017-08-08 02:37:16 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name
+    "#{@name}".strip
+  end
+
+end
