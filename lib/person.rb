8e49754bcedcd38cf1e0d2ca76d1ebf2e2b513fb
Edward Beazer <eddiebeazer@gmail.com>
2018-03-08 16:58:05 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  def initialize(name)
+  @name = name
+end
+   def name
+     return @name
+   end
+   def job
+     return @job
+   end
+
+   def name=(name)
+     @name = name
+   end
+
+   def job=(job)
+     @job = job
+   end
+ end
