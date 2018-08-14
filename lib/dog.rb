620324cc50684c2f3a3cfa259b2dabbd02624860
Byambaa Bayarmandakh <byambaa0325@gmail.com>
2018-08-14 06:14:33 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+  
+  def initialize(_name, _breed="Mutt")
+    @name= _name
+    @breed= _breed
+    
+  end
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person
+  
+  def initialize(_name)
+    @name= _name
+  end
+  
+  
+
+end
