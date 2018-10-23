ed6c4b354937d66559ee79a9e1b54c261c29ee9a
Florin Mocanu <florin.mocanu.studenti@isii.it>
2018-10-23 16:55:36 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+
+class Dog
+
+def initialize(name, breed="Mutt")
+  @name=name
+  @breed=breed
+end
+
+
+def name=(dog_name)
+  @name=dog_name
+end
+
+def name
+  @name
+end
+
+def breed=(breed_name)
+  @breed=breed_name
+end
+
+def breed
+  @breed
+end
+
+end
