daf734657de22d18d5767b42b2b0a355a0e0dbb3
Ben Francisco <bpfranci@math.tamu.edu>
2019-08-20 03:42:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+# Dog - class for a dog
+# name - name of the dog
+# breed - breed of the dog (default: Mutt)
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+# Person - class for a person
+# -> name - name of the person
+class Person
+  def initialize(name)
+    @name = name
+  end
+end
